d313a717671a29197f9a55c4402c288038b4bd99
Artemmal <45432448+Artemmal@users.noreply.github.com>
2019-11-19 15:27:05 +0200

    Add files via upload

---
+++ Program(10).cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace HW23._4
+{
+    abstract class Figure
+    {
+        public abstract void Type();
+    }
+    class Circle : Figure
+    {
+        public override void Type()
+        {
+            Console.WriteLine("Circle");
+        }
+    }
+    class Square : Figure
+    {
+        public override void Type()
+        {
+            Console.WriteLine("Square");
+        }
+    }
+    class Rectangle : Figure
+    {
+        public override void Type()
+        {
+            Console.WriteLine("Rectangle");
+        }
+    }
+
+    abstract class Colour
+    {
+        protected Figure figure;
+        public Figure Figure
+        {
+            set { figure = value; }
+        }
+        public Colour(Figure fig)
+        {
+            figure = fig;
+        }
+        public virtual void FigureType()
+        {
+            figure.Type();
+        }
+        public abstract void Paint();
+    }
+
+    class Red : Colour
+    {
+        public Red(Figure fig) : base(fig)
+        {
+        }
+        public override void Paint()
+        {
+            Console.WriteLine("Red");
+        }
+    }
+
+    class Blue : Colour
+    {
+        public Blue(Figure fig) : base(fig)
+        {
+        }
+        public override void Paint()
+        {
+            Console.WriteLine("Blue");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Colour red = new Red(new Circle());
+            red.FigureType();
+            red.Paint();
+            red.Figure = new Square();
+            red.FigureType();
+            red.Paint();
+            Colour blue = new Blue(new Rectangle());
+            blue.FigureType();
+            blue.Paint();
+            blue.Figure = new Square();
+            blue.FigureType();
+            blue.Paint();
+        }
+    }
+}
